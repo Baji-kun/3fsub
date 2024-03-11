e3a227151c83411cf47f589d83c213be369ec4cf
Baji-kun <152496758+Baji-kun@users.noreply.github.com>
2024-03-11 06:53:55 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -21,7 +21,7 @@ async def is_subscribed(filter, client, update):
 
     member_status = ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER
 
-    for channel_id in [FORCESUB_CHANNEL, FORCESUB_CHANNEL2, FORCESUB_CHANNEL3]:
+    for channel_id in [FORCESUB_CHANNEL, FORCESUB_CHANNEL2, FORCESUB_CHANNEL3, FORCESUB_CHANNEL4]:
         if not channel_id:
             continue
 
